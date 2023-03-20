2a6b723c5aca8ce9fedb7ea783f2179da4ca2ccd
StefanieKrohmann <stefanie.krohmann@hotmail.com>
2023-03-20 14:39:41 +0100

    Update App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import * as React from "react";
 import AppRouter from "./components/routing/AppRouter";
 
-
-export default function App() {
-  const drawerWidth = 220; 
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <Navbar/>
-      <CoursePage/>
-    </Box>
-  );
-}
-
-/*
 export default function App() {
   return (
     <AppRouter />
   );
-}*/
+}
