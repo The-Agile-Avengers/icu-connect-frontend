3e67cd64baf6bf44ae3df6782134947932107568
Tarek <tarekalakmeh@hotmail.ch>
2023-03-26 01:50:16 +0100

    Fix Layout Component

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,7 +1,7 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import theme from "../../design/theme";
-import Navbar from "../Navbar/Navbar";
+import Navbar from "../navbar/Navbar";
 import Box from "@mui/material/Box";
 import { PageTitle } from "../../design/typography";
 
@@ -14,23 +14,23 @@ export type { IProps };
 
 const Layout: React.FC<IProps> = ({ children, title }: IProps) => (
   <ThemeProvider theme={theme}>
-    <Navbar />
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
     <Box sx={{ display: "flex" }}>
-      {/* Navigation Bar */}
       <Navbar />
-      {/* defines padding of inner page */}
-      <Box component="main" sx={{ flexGrow: 1, p: 6, heigth: "100%" }}>
+      <CssBaseline />
+      <Box sx={{ display: "flex", flexDirection: "column", p: 3 }}>
         <PageTitle title={title} />
-        {/* ACTUAL PAGE CONTENT */}
-        {children}
+        <Box
+          component="main"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          {children}
+        </Box>
       </Box>
     </Box>
   </ThemeProvider>
 );
 
-/*
- * Don't forget to export your component!
- */
 export default Layout;
