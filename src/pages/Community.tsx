07de4e6ff695f66d40346883bc1d52999df5cf27
Tarek <tarekalakmeh@hotmail.ch>
2023-04-08 21:53:17 +0200

    add join/leave functionality, add auto scope

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -9,6 +9,9 @@ import {
   AccordionSummary,
   Typography,
   AccordionDetails,
+  Button,
+  Tabs,
+  Tab,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { UserModel } from "../models/UserModel";
@@ -58,6 +61,12 @@ interface RatingsResponse {
   content: Rating[];
 }
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
 const Community: React.FC = () => {
   const { id } = useParams<CommunityPageParams>() || "";
   const [error, setError] = useState<number>(1);
@@ -68,6 +77,49 @@ const Community: React.FC = () => {
   const addCommunityRating = (rating: Rating) => {
     setCommunityRatings([...communityRatings, rating]);
   };
+
+  /* Tab Navigation Logic */
+  const [activeTab, setActiveTab] = React.useState(0);
+  const [expandCommunityInfo, setExpandCommunityInfo] = React.useState<
+    boolean | null
+  >(null);
+
+  const handleChange = (event: React.SyntheticEvent, newTab: number) => {
+    setActiveTab(newTab);
+  };
+
+  function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <Box
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        sx={{ width: "100%" }}
+        {...other}
+      >
+        {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      </Box>
+    );
+  }
+
+  async function joinLeaveCommunity() {
+    if (id) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+      const { data } = await api.put<CommunityModel>(
+        `/users/communities/${id}`
+      );
+      /* ToDo: Backend is sending inconsistent responses
+      i.e. if user joins the course, data be a fully loaded CommunityModel
+           if user leaves the course, data will be null instead of fully loaded CommunityModel
+      */
+      setCommunityInfo({ ...communityInfo, joined: !communityInfo.joined });
+      setActiveTab(communityInfo.joined ? 1 : 0);
+      setExpandCommunityInfo(!expandCommunityInfo);
+    }
+  }
 
   async function getCommunityRatings() {
     if (id) {
@@ -99,6 +151,8 @@ const Community: React.FC = () => {
       try {
         const { data } = await api.get<CommunityModel>(`/communities/${id}`);
         setCommunityInfo(data);
+        setExpandCommunityInfo(!data.joined);
+        setActiveTab(data.joined ? 0 : 1);
         setError(0);
         return data;
       } catch (error) {
@@ -123,68 +177,100 @@ const Community: React.FC = () => {
 
   return error === 0 && id ? (
     <Layout>
-      <Accordion sx={{ bgcolor: "secondary.main", p: 3, mb: 3 }}>
+      <Accordion
+        expanded={expandCommunityInfo ?? !communityInfo.joined}
+        sx={{ bgcolor: "secondary.main", p: 3, mb: 3 }}
+        onChange={() => setExpandCommunityInfo(!expandCommunityInfo)}
+      >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography variant="h1" sx={{ mb: 0 }}>
-            {communityInfo ? communityInfo.name : "loading"}
-          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%",
+              pr: 3,
+            }}
+          >
+            <Typography variant="h1" sx={{ mb: 0 }}>
+              {communityInfo.name}
+            </Typography>
+            <Button
+              variant={communityInfo.joined ? "outlined" : "contained"}
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              onClick={joinLeaveCommunity}
+            >
+              {communityInfo.joined ? "Leave" : "Join"}
+            </Button>
+          </Box>
         </AccordionSummary>
         <AccordionDetails>
           <CommunityInfo community={communityInfo} />
         </AccordionDetails>
       </Accordion>
 
-      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-        <Box
-          title="Posts"
-          sx={{
-            bgcolor: "secondary.main",
-            p: 3,
-            flex: "1 0 500px",
-          }}
-        >
-          {Array(3)
-            .fill(1)
-            .map((i: number) => (
-              <CommunityPost
-                key={`A${i * Math.random()}`}
-                account={userExample}
-                title="I have a question"
-                postText="This is my question"
-                time="6 minutes"
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          bgcolor: "secondary.main",
+          width: "100%",
+        }}
+      >
+        <Box sx={{ borderBottom: 1, borderColor: "divider", width: "100%" }}>
+          <Tabs
+            value={activeTab}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Posts" sx={{ p: 3 }} />
+            <Tab label="Ratings" sx={{ p: 3 }} />
+          </Tabs>
+        </Box>
+
+        <TabPanel value={activeTab} index={0}>
+          <Box sx={{ width: "100%", alignItems: "stretch" }}>
+            {Array(3)
+              .fill(1)
+              .map((i: number) => (
+                <CommunityPost
+                  key={`A${i * Math.random()}`}
+                  account={userExample}
+                  title="I have a question"
+                  postText="This is my question"
+                  time="6 minutes"
+                />
+              ))}
+            <CommunityPostForm courseId={id || ""} />
+          </Box>
+        </TabPanel>
+        <TabPanel value={activeTab} index={1}>
+          <Box sx={{ width: "100%", alignItems: "stretch" }}>
+            {communityRatings.map((rating: Rating) => (
+              <CommunityRating
+                key={rating.id}
+                user={{
+                  id: 123,
+                  name: "WaitingForBackend",
+                  email: "ToDo@waitingforbackend.ch",
+                  avatar: imgLink,
+                }}
+                ratingContent={rating.content}
+                ratingTeaching={rating.teaching}
+                ratingWorkload={rating.workload}
+                textRating={rating.text}
+                time="22.3.2023"
               />
             ))}
-          <CommunityPostForm courseId={id || ""} />
-        </Box>
-        <Box
-          title="Ratings"
-          sx={{ bgcolor: "secondary.main", p: 3, flex: "1 0 500px" }}
-        >
-          {communityRatings.map((rating: Rating) => (
-            <CommunityRating
-              key={rating.id}
-              user={{
-                id: 123,
-                name: "WaitingForBackend",
-                email: "ToDo@waitingforbackend.ch",
-                avatar: imgLink,
-              }}
-              ratingContent={rating.content}
-              ratingTeaching={rating.teaching}
-              ratingWorkload={rating.workload}
-              textRating={rating.text}
-              time="22.3.2023"
+            <CommunityRatingForm
+              id={id}
+              addCommunityRating={addCommunityRating}
             />
-          ))}
-          <CommunityRatingForm
-            id={id}
-            addCommunityRating={addCommunityRating}
-          />
-        </Box>
+          </Box>
+        </TabPanel>
       </Box>
     </Layout>
   ) : error === 404 ? (
