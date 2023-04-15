59091dca61b7f97c1de95fa5389876fd00b612fb
Tarek <tarekalakmeh@hotmail.ch>
2023-04-15 17:43:33 +0200

    Update AppRouter

--- src/components/routing/AppRouter.tsx
+++ src/components/routing/AppRouter.tsx
@@ -6,7 +6,7 @@ import MyCommunities from "../../pages/MyCommunities";
 import Communities from "../../pages/Communities";
 import SignUp from "../../pages/SignUp";
 import { isLoggedIn } from "../../utils/utils";
-import CreateCommunity from "pages/CreateCommunity";
+import CreateCommunity from "../../pages/CreateCommunity";
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -17,7 +17,7 @@ const AppRouter = () => (
           <Route index path="/" element={<MyCommunities />} />
           <Route index path="/community/:id?" element={<Community />} />
           <Route index path="/community/create" element={<CreateCommunity />} />
-          <Route index path="/communities" element={<Search />} />
+          <Route index path="/communities" element={<Communities />} />
           <Route path="/login" element={<Navigate to="/" replace />} />
           <Route path="/signup" element={<Navigate to="/" replace />} />
         </>
