54fa39653db66347369c89a50d07777ade17ad6a
Tarek <tarekalakmeh@hotmail.ch>
2023-05-22 00:52:09 +0200

    change backend port

--- src/utils/getDomain.ts
+++ src/utils/getDomain.ts
@@ -3,8 +3,8 @@ const isProduction = () => process.env.NODE_ENV === "production";
 
 export const getDomain = () => {
   // AWS URL const prodUrl = "http://DEV-PROD-BALANCER-1363659248.eu-west-1.elb.amazonaws.com:8080";
-  const prodUrl =  `http://localhost:40797`;
-  const devUrl = `http://localhost:40797`;
+  const prodUrl =  `http://localhost:8080`;
+  const devUrl = `http://localhost:8080`;
 
   return isProduction() ? prodUrl : devUrl;
 };
